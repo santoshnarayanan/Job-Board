f9a9c6c54decf8c46734e8c2bd40e48d6aeef083
Santosh Narayanan <santosh33@gmail.com>
2023-10-25 03:44:05 +0200

    Custom Data replaced with  data from db

--- resolvers.js
+++ resolvers.js
@@ -1,8 +1,12 @@
+import { getJobs } from "./db/jobs.js";
 export const resolvers = {
     Query: {
-        jobs: () => 
+        jobs: async() => 
         {
-            return [
+            const jobs = await getJobs();
+            console.log(jobs);
+            return jobs;
+            /* return [
                 {
                     id: 'test-job1',
                     title: 'Accountant',
@@ -13,7 +17,7 @@ export const resolvers = {
                     title: 'Carpenter',
                     description: 'Create wooden tables and chairs',
                 },
-            ]
+            ] */
         }
     },
 };
