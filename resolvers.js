5e7e29d7d35a7d659790b4fdfbba5035c12f85d3
Santosh Narayanan <santosh33@gmail.com>
2023-10-25 03:46:24 +0200

    Code refactor

--- resolvers.js
+++ resolvers.js
@@ -1,23 +1,6 @@
 import { getJobs } from "./db/jobs.js";
 export const resolvers = {
     Query: {
-        jobs: async() => 
-        {
-            const jobs = await getJobs();
-            console.log(jobs);
-            return jobs;
-            /* return [
-                {
-                    id: 'test-job1',
-                    title: 'Accountant',
-                    description: 'Prepare company B/S',
-                },
-                {
-                    id: 'test-job2',
-                    title: 'Carpenter',
-                    description: 'Create wooden tables and chairs',
-                },
-            ] */
-        }
+        jobs: () => getJobs(),
     },
 };
