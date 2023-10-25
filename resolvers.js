227833108867850a348cbfd5c219ac31a6248025
Santosh Narayanan <santosh33@gmail.com>
2023-10-25 04:04:45 +0200

    Field Resolver

--- resolvers.js
+++ resolvers.js
@@ -3,4 +3,15 @@ export const resolvers = {
     Query: {
         jobs: () => getJobs(),
     },
+
+    Job:{
+        date: (job) => {
+            console.log(job);
+            return toIsoDate(job.createdAt);
+        }
+    }
 };
+
+function toIsoDate(value){
+    return value.slice(0,'yyyy-MM-dd'.length);
+}
